4d60684497ed896361df81b1f954d6caa97ce9df
eman796 <emanuelcalvopizarro0123@gmail.com>
2023-10-31 19:40:22 -0600

    Commit inicial  - 31 de julio a las 19:00 Menú listo En el metodo agregar, en teoría, ya puede guardar los datos Los métodos EliminarDatos y ListarTodo están vacíos (Declarados,pero sin nada dentro)

--- Lista.h
+++ Lista.h
@@ -8,32 +8,48 @@
 
 class Lista {
 public:
-    void agregar(){
-        std::string titulo ="";
-        char desc = char(250);
-        std::string tareaCompletada = "";
-        std::string fechaRegistro ="";
-        char fechacom = char(250);
-        std::cout <<"Ingrese el nombre que desea ponerle a la tarea (Título)\n";
-        std::cin>>titulo;
-        std::cout <<"Ingresa la descripcion de la tarea (De que trata y eso)\n" << std::endl;
-        std::cin>>desc;
-        std::cout <<"Ingrese la fecha de registro de la tarea\n"<< std::endl;
-        std::cin>>fechaRegistro;
-        std::cout <<"Ingrese la tarea completadas\n"<< std::endl;
-        std::cin>>tareaCompletada;
-        std::cout <<"Ingrese la fecha en que la tarea se completó\n"<< std::endl;
-        std::cin>>fechacom;
-    }
+    Lista();
+    ~Lista();
+    void agregar();
 
-    void EliminarDatos() {
+    void EliminarDatos() ;
 
-    }
-
-    void ListarTodo() {
-
-    }
+    void ListarTodo();
 };
+Lista::Lista(){
 
+};
+Lista::~Lista(){
+
+};
+struct Tarea{
+    std::string titulo ="";
+    std::string desc;
+    std::string tareaCompletada = "";
+    std::string fechaRegistro ="";
+    std::string fechacom = "";
+    struct Tarea *siguiente;
+};
+struct Tarea *Nodo_Siguiente, * Nodo_Anterior, *Tareas;
+void agregar(){
+    Tareas = new (Tarea);
+    std::cout <<"Ingrese el nombre que desea ponerle a la tarea (Título)\n";
+    std::getline(std::cin,Tareas->titulo);
+    std::cout <<"Ingresa la descripcion de la tarea (De que trata y eso)\n" << std::endl;
+    std::getline(std::cin,Tareas->desc);
+    std::cin.ignore();
+    std::cout <<"Ingrese la fecha de registro de la tarea\n"<< std::endl;
+    std::getline(std::cin,Tareas->fechaRegistro);
+    std::cout <<"Ingrese la tarea completadas\n"<< std::endl;
+    std::getline(std::cin,Tareas->tareaCompletada);
+    std::cout <<"Ingrese la fecha en que la tarea se completó\n"<< std::endl;
+    std::getline(std::cin,Tareas->fechacom);
+}
+void EliminarDatos(){
+
+}
+void ListarDatos(){
+
+}
 
 #endif //EXAMEN1_LISTA_H
--- menu.cpp
+++ menu.cpp
@@ -3,16 +3,15 @@
 Lista lis;
 int main() {
 int opcion;
-int argumrnto = 0;
 while (opcion !=4) {
-    system("cls");
     system("color 2");
-    std::cout << "Exámen 1 estructura de datos\n"
+    system("cls");
+    std::cout << "Examen 1 estructura de datos\n"
                  "1. Insertar datos\n"
                  "2.Eliminar datos\n"
                  "3. Listar los datos\n"
                  "4. Salir\n"
-                 "Digite una opción\n";
+                 "Digite una opcion\n";
     std::cin >> opcion;
     switch (opcion) {
         case 1:
